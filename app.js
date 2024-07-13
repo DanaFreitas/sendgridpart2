1e18c06f49323ac5aae81e4863ddc1760edb8857
Dana Freitas <danarobertfreitas@gmail.com>
2024-07-13 01:46:41 -0400

    Cleaned up the code

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-// //The good ones
 
 import url from "node:url";
 import path from "path";
 import { fileURLToPath } from "url";
-//does this not matter
+  //Not sure why the value is never read. 
 import express from "express";
 import bodyParser from "body-parser";
-//import { MailService } from "@sendgrid/mail";
 import sgMail from '@sendgrid/mail';
 import process from 'node:process';
-//import { loadEnvFile } from 'node:process';
-//sgMail.setApiKey(process.env.API_KEY);
 import dotenv from 'dotenv';
 dotenv.config();
 
 const __filename = url.fileURLToPath(import.meta.url);
-//const __dirname = import.meta.dirname;
 const __dirname = import.meta.filename;
 
-const PORT = 3000; // Defining PORT
+//const PORT = 3000; // Defining PORT
+  //This was for the localhost. 
 
-//const sgMail = new MailService();
-
-//sendgridClient.setApiKey(process.env.SENDGRID_API_KEY || "");
-//doubt ill need
-//process.
-//loadEnvFile();
 const apikey = process.env.API_KEY;
 
 
@@ -40,20 +30,15 @@ app.get("/", (req, res) => {
   res.end();
 });
 
-//app.listen(PORT, () => {
- //// console.log(`Server running on port ${PORT}`);
-  
-//});
 
 app.post("/submit", (req, res) => {
 console.log(`${apikey} is the apikey`)
   sgMail.setApiKey(apikey)
 
-  //  sgMail.setApiKey(process.env.API_KEY);
   
   const msg = {
     to: req.body.To,
-    from: req.body.From, // Use the email address or domain you verified above
+    from: req.body.From, 
     subject: req.body.Subject,
     text: req.body.Message,
     html: req.body.Message,
@@ -68,20 +53,3 @@ console.log(`${apikey} is the apikey`)
       console.error(error);
     });
 });
-
-//  require("dotenv").config();
-
-//  const express = require("express");
-//  const app = express();
-
-//  const httpServer = require('http').createServer(app);
-//  let PORT;
-//  //console.log(PORT)
-//  process.env.STATUS === 'production'
-// console.log(`${process.env.STATUS} is the process env status`)
-//  ?(PORT = process.env.PROD_PORT)
-//  :(PORT = process.env.DEV_PORT)
-//  console.log(`Test ${process.env.STATUS} test :${PORT}`)
-//  httpServer.listen(PORT, () => {
-//    console.log(`Server in ${process.env.STATUS} mode, listening on *:${PORT}`)
-//  });
