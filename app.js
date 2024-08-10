48a35b0b68f3ede2c00c49dc28b1063d028edbd6
Dana Freitas <danarobertfreitas@gmail.com>
2024-08-10 05:09:57 -0400

    added www.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "sendgridtesting.uk"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "www.sendgridtesting.uk"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
