7bf0e3a2313acd26228cb9567d39d7c9c2ec1ae2
Dana Freitas <danarobertfreitas@gmail.com>
2024-08-10 05:03:09 -0400

    Trying to add crossorigin stuff

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ dotenv.config();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "sendgridtesting.uk"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
