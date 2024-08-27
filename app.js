afebd9dfc6eeb40fabd22bd6d3c691e59639dea1
Dana Freitas <danarobertfreitas@gmail.com>
2024-08-27 12:39:04 -0400

    altered app.listen

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const __dirname = path.dirname(__filename)
 const apikey = process.env.API_KEY;
 
 
-  // sgMail.setApiKey(apikey)
+//   sgMail.setApiKey(apikey)
 
 
 app.use(express.static(path.join(__dirname)));
@@ -52,54 +52,53 @@ app.get("/", (req, res) => {
 
 
 
-//app.listen(PORT, () => {
-//console.log(`Server running on port ${PORT}`);
-//})
+app.listen(PORT, '0.0.0.0', () => {
+console.log(`Server running on port ${PORT}`);
+})
 
 
-//  const msg = {
-//   to:'danarobertfreitas@gmail.com',
-//   from: 'danarobertfreitas@gmail.com',
-//   subject: 'Message',
-//   text: 'Hello. To all of those who are reading, I hope this test works. It has been many months, but I am sure that I will prevail.',
-//   html: '<h1>Hello. To all of those who are reading, I hope this test works. It has been many months, but I am sure that I will prevail.</h1>',
-//  };
-// console.log(typeof 'msg')
-// console.log(`The message is ${msg}`)
-//  sgMail
-//  .send(msg)
-//  .then((response) => {
-// console.log(response[0].statusCode);
-// console.log(response[0].headers);
-// console.log(msg)    })
-// .catch((error) => {
-// console.error(error),
-// console.error('Error sending email:', error.response.body);
-// });
+//   const msg = {
+//    to:'danarobertfreitas@gmail.com',
+//    from: 'danarobertfreitas@gmail.com',
+//    subject: 'Message',
+//    text: 'Hello. To all of those who are reading, I hope this test works. It has been many months, but I am sure that I will prevail.',
+//    html: '<h1>Hello. To all of those who are reading, I hope this test works. It has been many months, but I am sure that I will prevail.</h1>',
+//   };
+//  console.log(typeof 'msg')
+//  console.log(`The message is ${msg}`)
+//   sgMail
+//   .send(msg)
+//   .then((response) => {
+//  console.log(response[0].statusCode);
+//  console.log(response[0].headers);
+//  console.log(msg)    })
+//  .catch((error) => {
+//  console.error(error),
+//  console.error('Error sending email:', error.response.body);
+//  });
 
 
  //This isnt part of the atutomatic trigger. 
  
  app.post("/", (req, res) => {
-   sgMail.setApiKey(apikey)
-  
+ sgMail.setApiKey(apikey)
+ 
    const msg = {
      to: "danarobertfreitas@gmail.com",  //req.body.To
-     from: "danarobertfreitas@gmail.com", 
-     replyTo: req.body.From,
-     subject: req.body.Subject,
-     text: req.body.Message,
-     html: req.body.Message,
+   from: "danarobertfreitas@gmail.com", 
+ replyTo: req.body.From,
+      subject: req.body.Subject,
+      text: req.body.Message,
+      html: req.body.Message,
    
-   };
-   sgMail
-     .send(msg)
-     .then((response) => {
-       console.log(response[0].statusCode);
-       console.log(response[0].headers);
- console.log(msg)    })
-     .catch((error) => {
-       console.error(error);
-     });
-   
- });
+    };
+    sgMail
+      .send(msg)
+      .then((response) => {
+        console.log(response[0].statusCode);
+        console.log(response[0].headers);
+  console.log(msg)    })
+      .catch((error) => {
+        console.error(error);
+      });   
+      });
